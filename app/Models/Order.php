83206829bd4f5f660d4afecc4f88bb04d41129c4
dvson-sun <dao.vinh.son@sun-asterisk.com>
2022-04-15 10:54:50 +0700

    seeder db

---
+++ app/Models/Order.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = [
+        'id',
+        'user_id',
+        'total_price',
+        'address',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class, 'order_products')->withPivot('quantity');
+    }
+}
