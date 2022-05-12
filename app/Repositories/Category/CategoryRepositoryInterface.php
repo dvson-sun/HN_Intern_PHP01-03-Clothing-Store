69fe1f13a2eca9a751ab8d692b2d56d70acb95d0
dvson-sun <dao.vinh.son@sun-asterisk.com>
2022-05-12 16:23:43 +0700

    Repository Category

---
+++ app/Repositories/Category/CategoryRepositoryInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Repositories\Category;
+
+use App\Repositories\RepositoryInterface;
+
+interface CategoryRepositoryInterface extends RepositoryInterface
+{
+    public function getCategoryList();
+
+    public function getCategory($id);
+
+    public function getParentCategory();
+
+    public function creatCategory($options);
+}
