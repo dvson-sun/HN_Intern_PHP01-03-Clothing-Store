ade456099ac39636a7a0e966124afdf20bb6f045
dvson-sun <dao.vinh.son@sun-asterisk.com>
2022-06-09 16:51:19 +0700

    Fix add Product

--- resources/views/admin/products/listproduct.blade.php
+++ resources/views/admin/products/listproduct.blade.php
@@ -6,7 +6,7 @@
     <div class="row">
         <div class="col-xs-12 col-md-12 col-lg-12">
             <div class="panel panel-primary">
-                <div class="panel-body">
+                <div class="panel-body mt-3">
                     @if ($message = Session::get('success'))
                     <div class="alert alert-success">
                         <p>{{ __($message) }}</p>
